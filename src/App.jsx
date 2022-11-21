0ed819d3170f351e110165ed450d2a2261f714db
AROSTEFHINA <113664246+AROSTEFHINA@users.noreply.github.com>
2022-11-21 11:48:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+const App = () => {
+  return (
+    <div className="mac-book-air-10">
+      <div className="image-106">
+        <span className="create-your-account">CREATE YOUR ACCOUNT</span>
+        <span className="email-phone">Email&#x2F;Phone</span>
+        <input type="text"/>
+        <span className="username">Username</span>
+        <input type="text"/>
+        <span className="pss">Password</span>
+        <input type="password"/>
+        <span className="cpss">Confirm Password</span>
+        <input type="text"/>
+        <div className="flex-container">
+          <input className="ch" type="checkbox"/>
+          <span className="terms-conditions">TERMS & CONDITIONS</span>
+        </div>
+        <button className="rectangle-11">
+          <span className="sign-up">SIGN UP</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+export default App;
